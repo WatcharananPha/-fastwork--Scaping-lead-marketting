e89204c0401a0aa86695a0bca72e27ca2002ead8
watcharanan phanmool <Kongla478@gmail.com>
2025-07-19 10:21:57 +0700

    Code Update

--- pages/3_LinkedIn_Scraping.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="LinkedIn Scraper", layout="wide")
-st.title("🔗 LinkedIn Scraping")
-st.markdown("---")
-st.info("LinkedIn scraping functionality is under development and will be available soon.")
-st.warning("Scraping LinkedIn is highly restricted and requires advanced techniques to avoid account suspension.")
